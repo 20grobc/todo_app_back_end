d355754e1d849551904d5f6fdd76c7843358bc13
Claire Grob <clairegrob@gmail.com>
2017-07-21 03:00:07 +0000

    add database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,22 @@
 class TodoController < ApplicationController
     def index
     end
-
+    def show
+        if params["id"] == "1"
+            @todo_description = "go to the store"
+            @todo_pomodoro_estimate = 4
+        
+        elsif params["id"] == "2"
+            @todo_description = "sleep"
+            @todo_pomodoro_estimate = 7
+        
+        elsif params["id"]== "3"
+            @todo_description= "eat"
+            @todo_pomodoro_estimate= 1
+            
+        elsif params["id"]== "4"
+            @todo_description= "be cool"
+            @todo_pomodoro_estimate= 10
+        end
+    end
 end
